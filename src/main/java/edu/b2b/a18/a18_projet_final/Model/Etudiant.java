484225a5d08ba602024787c2e99963c4d0e1c330
Mourad Chikhi <6286227@bdeb.qc.ca>
2025-04-05 00:06:47 -0400

    Initial Commit

--- src/main/java/edu/b2b/a18/a18_projet_final/Model/Etudiant.java
+++ src/main/java/edu/b2b/a18/a18_projet_final/Model/Etudiant.java
@@ -1,4 +1,69 @@
 package edu.b2b.a18.a18_projet_final.Model;
 
 public class Etudiant {
+
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String specialite;
+
+    public Etudiant(int id, String nom, String prenom, String email, String specialite) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.specialite = specialite;
+    }
+    public Etudiant(){
+    }
+    public Etudiant(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSpecialite() {
+        return specialite;
+    }
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", specialite='" + specialite + '\'' +
+                '}';
+    }
 }
+
